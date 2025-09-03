c8cfb76376d61e75fd3a3dcbc87eb36428de91bd
deeganm665-gif <deegan.m665@stu.nebo.edu>
2025-09-03 14:18:27 -0600

    Added Boxes and scuffed Jump

--- Tank game - Prototype 1/Assets/Scripts/Player_Move.cs
+++ Tank game - Prototype 1/Assets/Scripts/Player_Move.cs
@@ -8,6 +8,7 @@ public class Player_move : MonoBehaviour
     public float rotspeed;
     public float hInput;
     public float vInput;
+    public float jInput;
     public float jumpforce;
     public Rigidbody playerRB;
 
@@ -15,9 +16,11 @@ public class Player_move : MonoBehaviour
     {
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
+        jInput = Input.GetAxis("Jump");
 
         transform.Rotate(Vector3.up, hInput * rotspeed * Time.deltaTime); //Move vehicle left and right
         transform.Translate(Vector3.forward * vInput * speed * Time.deltaTime); //Move vehicle forward and back
+        transform.Translate(Vector3.up * jInput * jumpforce * Time.deltaTime); //Scuffed jump
 
     }
 }
